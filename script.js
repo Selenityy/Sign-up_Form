28d361314f026bbe5f554273a101632f98347e6b
Selena <selena.k.rodriguez@gmail.com>
2023-01-10 19:23:47 -0600

    Added a paragraph for the error message

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let password = document.getElementById("password");
 let confirm_password = document.getElementById("confirm_password");
-let input = document.getElementById("error_message").innerHTML;
+let message = document.getElementById("error_message").innerHTML;
 
 function check (input) {
 	if (confirm_password.value != password.value) {
 		console.log("password did not match");
-        input.setCustomValidity("Password must be matching");
+        message.setCustomValidity("Password must be matching");
 	} else {
         console.log("password matched");
 		return;
